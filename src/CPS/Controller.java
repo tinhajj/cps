729c54d44d1461be135d025411dc95e109ea8b10
Thanoj Singh <thanoj.singh@gmail.com>
2019-08-06 23:22:34 -0400

    make use of fxml to make gui, use controller for the click handler

--- src/CPS/Controller.java
+++ src/CPS/Controller.java
@@ -1,4 +1,35 @@
 package CPS;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private ClickCounter clickCounter;
+
+    @FXML
+    Text totalClicksText;
+
+    @FXML
+    Text leftClicksText;
+
+    @FXML
+    Text rightClicksText;
+
+    public void initialize(URL location, ResourceBundle resource) {
+        StopWatchFactory stopWatchFactory = new StopWatchFactory(StopWatchType.Milli);
+        clickCounter = new ClickCounter(stopWatchFactory);
+    }
+
+    public void handleClick(MouseEvent event) {
+        clickCounter.countClick(event);
+
+        totalClicksText.setText("Total Clicks: " + (clickCounter.totalClicksPerSecond()));
+        leftClicksText.setText("Left Clicks: " + (clickCounter.getLeftClicksPerSecond()));
+        rightClicksText.setText("Right Clicks: " + (clickCounter.getRightClicksPerSecond()));
+    }
 }
--- src/CPS/Main.java
+++ src/CPS/Main.java
@@ -1,57 +1,25 @@
 package CPS;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        StopWatchFactory stopWatchFactory = new StopWatchFactory(StopWatchType.Milli);
-        ClickCounter clickCounter = new ClickCounter(stopWatchFactory);
+    public void start(Stage primaryStage) throws java.io.IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
 
-        Text totalClicksText = new Text();
-        totalClicksText.setText("Total Clicks: ");
-
-        totalClicksText.setX(50);
-        totalClicksText.setY(50);
-
-        Text rightClicksText = new Text();
-        rightClicksText.setText("Right Clicks: ");
-
-        rightClicksText.setX(50);
-        rightClicksText.setY(80);
-
-        Text leftClicksText = new Text();
-        leftClicksText.setText("Left Clicks: ");
-
-        leftClicksText.setX(50);
-        leftClicksText.setY(100);
-
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group(totalClicksText, rightClicksText, leftClicksText);
+        Parent root = loader.load();
 
         primaryStage.setTitle("CPS");
 
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        scene.setOnMouseClicked(
-                (MouseEvent event) -> {
-                    clickCounter.countClick(event);
-
-                    leftClicksText.setText("Left Clicks: " + (clickCounter.getLeftClicksPerSecond()));
-                    rightClicksText.setText("Right Clicks: " + (clickCounter.getRightClicksPerSecond()));
-                    totalClicksText.setText("Total Clicks: " + (clickCounter.totalClicksPerSecond()));
-                }
-        );
     }
-
 
     public static void main(String[] args) {
         launch(args);
